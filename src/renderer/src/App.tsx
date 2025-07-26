8c0863e7e3a5b95c6779ae97065ac8155b92d81e
smilewilson1999 <smilewilson1999@gmail.com>
2025-07-26 18:05:48 -0400

    Replaced: MDX -> @BlockNote; Cafe -> Mono

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import { Content, FloatingNoteTitle, NotePreviewList, RootLayout, Sidebar } from './components'
 import { ActionButtonsRow } from './components/ActionButtonRow'
 import { DraggableTopBar } from './components/DraggableTopBar'
-import { MarkdownEditor } from './components/MarkdownEditor'
+import { NoteEditor } from './components'
 
 function App(): React.JSX.Element {
   return (
@@ -12,9 +12,9 @@ function App(): React.JSX.Element {
           <ActionButtonsRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="p-8 border-l border-l-gray-300">
+        <Content className="border-l border-l-gray-300">
           <FloatingNoteTitle className="pt-2 app-title" />
-          <MarkdownEditor />
+          <NoteEditor />
         </Content>
       </RootLayout>
     </>
--- src/renderer/src/components/AppLayout.tsx
+++ src/renderer/src/components/AppLayout.tsx
@@ -12,7 +12,7 @@ export const RootLayout = ({ children, className, ...props }: ComponentProps<'ma
 export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
   return (
     <aside
-      className={twMerge('w-[250px] mt-10 h-[100vh + 10px] overflow-auto', className)}
+      className={twMerge('sidebar mt-10 h-[100vh + 10px] overflow-auto', className)}
       {...props}
     >
       {children}
@@ -22,7 +22,11 @@ export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside
 
 export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
   ({ children, className, ...props }, ref) => (
-    <div ref={ref} className={twMerge('flex-1 overflow-auto', className)} {...props}>
+    <div
+      ref={ref}
+      className={twMerge('flex-1 overflow-auto pt-[var(--header-height)]', className)}
+      {...props}
+    >
       {children}
     </div>
   )
